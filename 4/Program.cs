41bff41ee64e0203c463357f6bfa082e6ddf9b14
ali <ali@MacBook-Pro-Ali.local>
2023-01-02 18:46:04 +0400

    mesaj

---
+++ 4/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _4;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int count = 0;
+        int sum = 0;
+
+        int[] numbers = { 5, 7, 10, 15, 90 };
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] % 5 == 0)
+            {
+                sum += numbers[i];
+                count++;
+               
+            }
+        }
+              Console.WriteLine(sum);
+        Console.WriteLine(count);
+
+
+
+
+
+
+    }
+}
+
